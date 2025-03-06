6c584d8b47cb850da03cf4c2c0a646f372f9f49c
sinach12 <osinachiamadi92@gmail.com>
2025-03-06 01:18:40 +0100

    Add files via upload

---
+++ amb.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();  // Prevent form submission
+    alert("Thank you for reaching out! We'll get back to you soon.");
+});
